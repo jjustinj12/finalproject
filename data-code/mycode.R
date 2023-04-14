2424c8c83ba5e1b8692b2973ef84ae4c8655afe9
jjustinj12 <justinjoseph@justins-mbp.wireless.emory.edu>
2023-04-14 17:34:11 -0400

    done with easy graphs

--- data-code/mycode.R
+++ data-code/mycode.R
@@ -5,7 +5,8 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, readr, readxl, scales, acs, tidyr)
 library(knitr)
 
-
+final.data<-read_tsv("data/output/acs_medicaid.txt")
+brfss.data <- read_csv('data/input/BRFSS/BRFSS_Data.csv')
 
 #so i need to figure out the number of states that expanded 
 #control group is equivalent to states that never expanded or expanded 
@@ -39,9 +40,8 @@ figure_1
 
 
 
-
 #importing data set
-brfss.data <- read_csv('data/input/BRFSS/BRFSS_Data.csv')
+
 colnames(brfss.data)
 view(brfss.data)
 
@@ -51,8 +51,62 @@ brfss.data.final <- brfss.data %>%
   select(Year, State, Class, Topic, Question, Response, Sample_Size, Data_value) %>%
   filter(Topic %in% c("Overall Health", "Depression", "Last Checkup", "Diabetes", "HIV Test", "Health Care Coverage"))%>%
   filter(Year>=2012 & Year<=2019)
-maxy<-brfss.data.final %>%
+
+head(subset(brfss.data.final, Topic == "Last Checkup")$Response)
+
+brfss_expanded<-brfss.data.final %>%
   filter(State %in% c("Arizona", "Arkansas", "California", "Colorado", "Delaware", "District of Columbia", 
                  "Connecticut", "Hawaii", "Illinois", "Iowa", "Kentucky", "Maryland", "Massachusetts", 
                  "Michigan", "Minnesota", "Nevada", "New Hampshire", "New Jersey", "New Mexico", 
                  "New York", "North Dakota", "Ohio", "Oregon", "Rhode Island", "Vermont", "Washington", "West Virginia"))
+
+brfss_not_expanded<-brfss.data.final %>%
+  filter(State %in% c("Alabama", "Florida", "Georgia", "Kansas", "Mississippi", 
+                      "North Carolina", "South Carolina", "South Dakota", "Tennessee",
+                      "Texas", "Wisconsin", "Wyoming"))
+
+
+
+health_expanded <- brfss_expanded %>%
+  group_by(Year) %>%
+  summarise(poorhealth_prop = sum(Response %in% c("Poor", "Fair"), na.rm = TRUE))
+health_expanded
+health_not_expanded<-brfss_not_expanded%>%
+  group_by(Year)%>%
+  summarise(poorhealth_prop = sum(Response %in% c("Poor", "Fair"), na.rm = TRUE) / n())
+health_not_expanded
+
+table1 <- left_join(health_expanded, health_not_expanded, by = "Year")
+
+table1
+
+
+depression_expanded <-brfss_expanded %>%
+  filter(Topic=="Depression")%>%
+  group_by(Year)%>%
+  summarise(depression_prop = sum(Response=="Yes", na.rm = TRUE))
+depression_expanded
+
+depression_not_expanded <-brfss_not_expanded %>%
+  filter(Topic=="Depression")%>%
+  group_by(Year)%>%
+  summarise(depression_prop = sum(Response=="Yes", na.rm = TRUE))
+depression_not_expanded
+
+
+checkup_expanded <-brfss_expanded %>%
+  filter(Topic=="Last Checkup")%>%
+  group_by(Year)%>%
+  summarise(depression_prop = sum(Response=="Within the past year", na.rm = TRUE))
+checkup_expanded
+
+checkup_not_expanded <-brfss_not_expanded %>%
+  filter(Topic=="Last Checkup")%>%
+  group_by(Year)%>%
+  summarise(depression_prop = sum(Response=="Within the past year", na.rm = TRUE))
+checkup_not_expanded
+
+
+count(brfss.data.final, Year==2012 & Response=="Poor")
+
+      
